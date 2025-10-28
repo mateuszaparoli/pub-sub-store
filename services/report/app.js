3d84809735de819f31e5b39a30990a13c1d50ea8
mateuszaparoli <mateuszaparoli7@gmail.com>
2025-10-28 18:40:26 -0300

    feat: add report service with RabbitMQ integration

--- services/report/app.js
+++ services/report/app.js
@@ -24,7 +24,32 @@ async function printReport() {
 }
 
 async function consume() {
-    //TODO: Constuir a comunicação com a fila 
+    try {
+        const service = await RabbitMQService.getInstance()
+        // consume messages from queue 'report'
+        await service.consume('report', async (msg) => {
+            if (!msg) return
+            try {
+                const content = JSON.parse(msg.content.toString())
+                // content is expected to be an object or an array of products
+                if (Array.isArray(content)) {
+                    await updateReport(content)
+                } else if (content && content.products) {
+                    await updateReport(content.products)
+                } else {
+                    // if the message itself is a product
+                    await updateReport([content])
+                }
+                await printReport()
+            } catch (err) {
+                console.error('Failed to process message from report queue:', err.message)
+            }
+        })
+        console.log('Report service is consuming messages from queue "report"')
+    } catch (err) {
+        console.error('Error connecting to RabbitMQ:', err.message)
+        process.exit(1)
+    }
 } 
 
 consume()
